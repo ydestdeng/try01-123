15828a2d90f15bc004d6d97d78d2853f376c8c67
ydestdeng <ydest.deng@gmail.com>
2017-06-01 11:24:57 +0800

    initial

---
+++ try01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  try01
+//
+//  Created by Yi-Shin on 2017/5/18.
+//  Copyright © 2017年 Yi-Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
